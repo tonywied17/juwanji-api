9e825c5e278b34229687606439e9c23f836c8775
Tony Wiedman <107732340+tonywied17@users.noreply.github.com>
2023-03-29 09:46:57 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
-var corsOptions = {
-  origin: "*"
-};
+const cors = require("cors");
 
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
+}));
 
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+//test
+app.use(express.static('resources'));
 
 // database
 const db = require("./app/models");
@@ -28,8 +31,8 @@ db.sequelize.sync();
 // });
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the start of something." });
+app.get("/juwanji", cors(), (req, res) => {
+  res.json({ message: "Juwanji API." });
 });
 
 // routes
@@ -37,7 +40,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
